f18ee6ce56a488c1d065b05b3cae76a2bcb25f17
Ozlem Korpe <25618760+ozlemkorpe@users.noreply.github.com>
2024-12-11 21:42:12 +0300

    Reset bar check

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -8,4 +8,7 @@ test('Check Home page elements', async ({ page }) => {
   
   // Check page heading
   await expect(page.getByRole('heading', { name: 'Invicti Testing Site' })).toBeVisible();
+  
+  //Check reset bar
+  await expect(page.locator('#resetbar')).toHaveText('This website is automatically reset at every midnight (00:00 - UTC).');
 });
