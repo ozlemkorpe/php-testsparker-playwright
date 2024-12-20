d32d35ad0d991454279808bcb49804f63cffca8c
Ozlem Korpe <25618760+ozlemkorpe@users.noreply.github.com>
2024-12-21 00:28:54 +0300

    Added invalid login scenarios

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -9,22 +9,16 @@ test('Check Login page elements', async ({ page }) => {
   
   // Check heading
   await expect(page.getByRole('heading', { name: 'Login Area' })).toBeVisible();
-
   // Check information text paragraph.
   await expect(page.locator('text=Enter your credentials (admin / admin123456)')).toBeVisible();
-
   // Check Username text
   await expect(page.locator('text=Username:')).toBeVisible();
-
   // Check Username input field
   await expect(page.locator('input[name="username"]')).toBeVisible();
-  
   // Check password text
   await expect(page.locator('text=Password:')).toBeVisible();
-
   // Check Password input field
   await expect(page.locator('input[name="password"]')).toBeVisible();
-
   // Check Submit button 
   await expect(page.locator('input[value="SUBMIT"]')).toBeVisible();
 });
@@ -33,13 +27,29 @@ test('Check Login page elements', async ({ page }) => {
 test('Fill the form and login', async ({ page }) => {
   // Fill username field
   await page.locator('input[name="username"]').fill('admin');
-
   // Fill password field
   await page.locator('input[name="password"]').fill('admin123456');
-
   // Click on Submit button 
   await page.locator('input[value="SUBMIT"]').click();
-
   //Check Page URL
   await expect(page).toHaveURL('http://php.testsparker.com/auth/internal.php');
+});
+
+
+test('Test login with invalid credentials', async({ page }) => {
+  //Fill username field
+  await page.locator('input[name="username"]').fill('ozlem');
+  // Fill password field
+  await page.locator('input[name="password"]').fill('incorrect');
+  // Click on Submit button 
+  await page.locator('input[value="SUBMIT"]').click();
+  //Check Page URL
+  await expect(page).not.toHaveURL('http://php.testsparker.com/auth/internal.php');
+});
+
+test('Test login without entering credentials', async({ page }) => {
+  // Click on Submit button 
+  await page.locator('input[value="SUBMIT"]').click();
+  //Check Page URL
+  await expect(page).not.toHaveURL('http://php.testsparker.com/auth/internal.php');
 });
